e2894fc1a6b66839ee5958345c84da31ea9fd34b
Burhan ELGÜN <belgun@gtu.edu.tr>
2018-02-06 11:52:58 +0300

    Add files via upload

---
+++ Node5.java
@@ -0,0 +1,36 @@
+package testing;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+public class Node5 {   
+    WebDriver driver;
+    String nodeUrl;
+  @Test
+  public void f() {
+        try {
+            //configuration
+            nodeUrl= "http://172.16.56.248:5555/wd/hub";
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            capabilities.setBrowserName("chrome");
+            capabilities.setPlatform(Platform.LINUX);
+            driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+           
+            //test scripts
+            driver.manage().deleteAllCookies();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
+            driver.get("https://www.google.com/");
+        } 
+        catch (MalformedURLException e) {       
+            e.printStackTrace();           
+        }
+  }
+}
